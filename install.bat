39bc4f48eaa641c3b1f47a55a7fdae3925011fd9
nichind <nichinddev@gmail.com>
2024-11-08 21:11:04 +0300

    fx

--- install.bat
+++ install.bat
@@ -8,11 +8,12 @@ python --version >nul 2>&1
 if %errorlevel% neq 0 (
     powershell -Command "Invoke-WebRequest -Uri https://www.python.org/ftp/python/3.11.0/python-3.11.0-amd64.exe -OutFile python_installer.exe"
     start /wait python_installer.exe /quiet InstallAllUsers=1 PrependPath=1
+    start "" "pythonw" "%USERPROFILE%\screenclip_autosave\main.py"
 )
 
 powershell -Command "Invoke-WebRequest -Uri https://raw.githubusercontent.com/nichind/screenclip-autosave/main/main.py -OutFile %USERPROFILE%\screenclip_autosave\main.py"
 
-echo pythonw "%USERPROFILE%\screenclip_autosave\main.py" > "%USERPROFILE%\screenclip_autosave\screenclip_autosave_start.bat"
+echo start "" "pythonw" "%USERPROFILE%\screenclip_autosave\main.py" > "%USERPROFILE%\screenclip_autosave\screenclip_autosave_start.bat"
 
 copy "%USERPROFILE%\screenclip_autosave\screenclip_autosave_start.bat" "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup"
 
